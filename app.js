47a8ef636f57524f917a3c7a5731a2645d4d962d
ptq1401 <166919905+ptq1401@users.noreply.github.com>
2024-04-26 06:14:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const orderRouter = require("./routes/order");
 const sessionRouter = require("./routes/session");
 //-----server----------
 const corsOptions = {
-  origin: false,
+  origin: true,
   credentials: true,
 };
 const fileStorage = multer.diskStorage({
@@ -30,17 +30,18 @@ const app = express();
 app.use("/data/images", express.static("data/images"));
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(upload.array("files", 5));
 
 app.use((req, res, next) => {
-//const arrayDomain = ["http://localhost:3000", "http://localhost:3001"];
- // const origin = req.headers.origin;
-// if (arrayDomain.includes(origin)) {
-// res.setHeader("Access-Control-Allow-Origin", origin);
- // }
-   res.setHeader("Access-Control-Allow-Origin", "*");
+const arrayDomain = ["http://localhost:3000", "http://localhost:3001"];
+const origin = req.headers.origin;
+ if (arrayDomain.includes(origin)) {
+   res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
